67d4f413e576b8755b3f67d4efc8b1611be5aefb
imranN74 <imrannaziransari69@gmail.com>
2024-11-23 23:44:59 +0530

    add scheme init for the project

---
+++ backend/prisma/migrations/20241123181034_schema_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Content" (
+    "id" TEXT NOT NULL,
+    "urlKey" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Content_pkey" PRIMARY KEY ("id")
+);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,6 @@ wss.on("connection", (ws) => {
   });
 
   ws.on("message", (data, isBinary) => {
-    console.log(data);
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
