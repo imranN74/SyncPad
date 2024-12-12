f1e0cb65b109c535bc76acbefe871d3057667596
imranN74 <imrannaziransari69@gmail.com>
2024-12-12 21:27:49 +0530

    update vite config to resolve require error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,22 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     commonjsOptions: {
+//       include: [/node_modules/],
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
+  optimizeDeps: {
+    exclude: ["yaml", "yocto-queue"], // Replace with problematic dependencies
   },
 });
