cc42c5c4157221a6b78fd930b8907440f4bc04c9
imranN74 <imrannaziransari69@gmail.com>
2025-03-29 23:01:52 +0530

    chanegs in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,12 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     commonjsOptions: {
-//       include: [/node_modules/],
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["yaml", "yocto-queue"], // Replace with problematic dependencies
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
   },
 });
